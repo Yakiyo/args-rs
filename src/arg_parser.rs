cb84c372d70bffa2c8aee1768c5e2de84ddaf17a
Yakiyo <yakiyo1206@gmail.com>
2023-09-30 04:59:35 +0000

    feat: add flags and options methods

--- src/arg_parser.rs
+++ src/arg_parser.rs
@@ -1,3 +1,82 @@
+use std::collections::HashMap;
+
+use crate::flag::{Flag, FlagType};
+
 /// A class for taking a list of raw command line arguments and parsing out
 /// options and flags from them.
-pub struct ArgParser {}
+#[derive(Debug, Default)]
+pub struct ArgParser {
+    flags: HashMap<String, Flag>,
+    allows_anything: bool,
+}
+
+impl ArgParser {
+    /// initiate a new arg parser
+    pub fn new() -> Self {
+        ArgParser::default()
+    }
+
+    pub fn allow_anything(&mut self, allow: bool) -> &Self {
+        self.allows_anything = allow;
+        self
+    }
+
+    pub fn add_flag(
+        &mut self,
+        name: String,
+        abbr: Option<char>,
+        help: Option<String>,
+        defaults_to: bool,
+        negatable: bool,
+    ) -> &Self {
+        // for flag types, we consider default value `true` when option is some,
+        // and default value `false` when option is none
+        let default = if defaults_to {
+            Some(String::new())
+        } else {
+            None
+        };
+        let flag = Flag {
+            name: name.clone(),
+            abbr,
+            help,
+            negatable,
+            default,
+            ..Default::default()
+        };
+        self.flags.insert(name, flag);
+        self
+    }
+
+    pub fn add_option(
+        &mut self,
+        name: String,
+        abbr: Option<char>,
+        help: Option<String>,
+        value_help: Option<String>,
+        possible_values: Option<Vec<String>>,
+        defaults_to: bool,
+        required: bool,
+    ) -> &Self {
+        // for flag types, we consider default value `true` when option is some,
+        // and default value `false` when option is none
+        let default = if defaults_to {
+            Some(String::new())
+        } else {
+            None
+        };
+        let flag = Flag {
+            name: name.clone(),
+            abbr,
+            help,
+            default,
+            value_help,
+            possible_values,
+            required,
+            flag_type: FlagType::Option,
+            ..Default::default()
+        };
+        self.flags.insert(name, flag);
+        self
+    }
+}
--- src/flag.rs
+++ src/flag.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, Default)]
 pub struct Flag {
     pub name: String,
     pub abbr: Option<char>,
@@ -11,18 +12,22 @@ pub struct Flag {
 }
 
 impl Flag {
+    /// if it is of type flag
     pub fn is_flag(&self) -> bool {
         matches!(self.flag_type, FlagType::Flag)
     }
 
+    /// if it is of type option
     pub fn is_option(&self) -> bool {
         matches!(self.flag_type, FlagType::Option)
     }
 }
 
-/// the type of the flag
+/// The type of the flag
+#[derive(Debug, Default)]
 pub enum FlagType {
     /// is of bool value, as in takes no value
+    #[default]
     Flag,
     /// takes a single value
     Option,
