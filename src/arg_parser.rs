1a48687130295d4013b3527ef9488931d7eb4cf6
Yakiyo <yakiyo1206@gmail.com>
2023-09-30 04:29:56 +0000

    feat: more work on the flag

---
+++ src/arg_parser.rs
@@ -0,0 +1,3 @@
+/// A class for taking a list of raw command line arguments and parsing out
+/// options and flags from them.
+pub struct ArgParser {}
---
+++ src/flag.rs
@@ -0,0 +1,28 @@
+pub struct Flag {
+    pub name: String,
+    pub abbr: Option<char>,
+    pub help: Option<String>,
+    pub value_help: Option<String>,
+    pub possible_values: Option<Vec<String>>,
+    pub negatable: bool,
+    pub flag_type: FlagType,
+    pub required: bool,
+}
+
+impl Flag {
+    pub fn is_flag(&self) -> bool {
+        matches!(self.flag_type, FlagType::Flag)
+    }
+
+    pub fn is_option(&self) -> bool {
+        matches!(self.flag_type, FlagType::Option)
+    }
+}
+
+/// the type of the flag
+pub enum FlagType {
+    /// is of bool value, as in takes no value
+    Flag,
+    /// takes a single value
+    Option,
+}
